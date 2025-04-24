b3ca5557e4041b9c4a2df047175272809d69e65d
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-24 19:46:29 +0545

    updated

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,14 @@
+'use client';
+
+import Link from "next/link";
+
+export default function Navbar() {
+    return(
+        <nav className="bg-gray-800 p-4 text-white shadow-md">
+            <div>
+                
+            </div>
+
+        </nav>
+    )
+}
