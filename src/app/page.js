4a61ceb60a89f5645082fc5f3b6da6443c8a8525
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-26 20:54:43 +0545

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function HomePage() {
   };
 
   },[])
-  const texts = [ "Welcome to Web3", "Your Assets You Control"];
+  const texts = [ "Welcome to Web3", "Your Assets Your Control"];
   
   return (
     <div className="bg-gray-900 min-h-screen text-white font-sans">
@@ -31,10 +31,10 @@ export default function HomePage() {
         
         <div className="h-48 mb-8 bg-gray-800 rounded-xl overflow-hidden relative">
           <div className="absolute inset-0 bg-gradient-to-r from-blue-900 to-purple-900"></div>
-          <div className="overflow-hidden white-space-nowrap py-4">
+          <div className="overflow-hidden white-space-nowrap py-4 mt-4">
             {/* <h2 className="text-5xl text-center ">Welcome to Web3</h2> */}
 
-            <div className="flex w-max animate-scroll gap-16 text-5xl font-semibold text-gray-900 dark:text-white" ref={sliderRef}>
+            <div className="flex w-max animate-scroll gap-16 text-4xl lg:text-8xl font-semibold text-gray-900 dark:text-white" ref={sliderRef}>
               {[...texts, ...texts].map((text, index) => (
                 <span key={index} className="px-4">{text}</span>
               ))}
