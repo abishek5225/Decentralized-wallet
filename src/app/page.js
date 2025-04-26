663421ec8b0e7a92bbe0fae5bd85512e2e2ba984
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-26 20:44:58 +0545

    infinite scroll animation added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,28 @@
 'use client';
 
+import { useEffect, useRef } from "react";
 export default function HomePage() {
+
+  const sliderRef = useRef(null);
+  useEffect(()=>{
+    const slider = sliderRef.current;
+    let animation;
+
+  const animate = ()=> {
+    slider.scrollleft += 1;
+    if (slider.scrollLeft >= slider.scrollWidth - slider.clientWidth) {
+      slider.scrollLeft = 0;
+    }
+    animation = requestAnimationFrame(animate);
+  }
+  animation = requestAnimationFrame(animate);
+  return () => {
+    cancelAnimationFrame(animation);
+  };
+
+  },[])
+  const texts = [ "Welcome to Web3", "Your Assets You Control"];
+  
   return (
     <div className="bg-gray-900 min-h-screen text-white font-sans">
       
@@ -9,9 +31,15 @@ export default function HomePage() {
         
         <div className="h-48 mb-8 bg-gray-800 rounded-xl overflow-hidden relative">
           <div className="absolute inset-0 bg-gradient-to-r from-blue-900 to-purple-900"></div>
-          <div className="absolute inset-0 p-6 flex flex-col justify-center">
-            <h2 className="text-5xl text-center text-gray-900 dark:text-white">Welcome to Web3</h2>
-            <p className="text-gray-400">Your Assets Your Control</p>
+          <div className="overflow-hidden white-space-nowrap py-4">
+            {/* <h2 className="text-5xl text-center ">Welcome to Web3</h2> */}
+
+            <div className="flex w-max animate-scroll gap-16 text-5xl font-semibold text-gray-900 dark:text-white" ref={sliderRef}>
+              {[...texts, ...texts].map((text, index) => (
+                <span key={index} className="px-4">{text}</span>
+              ))}
+            </div>
+            
           </div>
         </div>
         </main>
