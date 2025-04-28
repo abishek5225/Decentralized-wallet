934b6eb93247d0080216e8e75c80aa5d735d07f8
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-28 18:45:03 +0545

    updated

--- src/app/wallets/page.js
+++ src/app/wallets/page.js
@@ -12,7 +12,16 @@ export default function Wallets() {
             </Head>
             <main className="max-w-4xl mx-auto py-12 px-4 sm:px6 lg:px-8">
                 <div className="text-center">
-                    <h1 className="text-3xl lg:text-7xl font-bold text-gray-100">Download GuardianChain</h1>
+                    <h1 className="text-3xl lg:text-7xl sm:text-5xl font-bold text-gray-100">Download GuardianChain</h1>
+                    <p className="text-gray-100 text-xl">Simple and Secure access to your digital assets.</p>
+                </div>
+                {/* download section */}
+
+                <div className="bg-white shadow rounded-lg overflow-hidden">
+                    <div className="flex  border-b">
+                        <button className={`flex-1 py-4 `}></button>
+
+                    </div>
                 </div>
 
             </main>
