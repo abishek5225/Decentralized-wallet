6efa00a1d61a69ed50832b9ffa38cef3e6f9513a
abishek5225 <abishekchaulagain5225@gmail.com>
2025-05-03 22:43:19 +0545

    updated

---
+++ src/app/BrowserWallet/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function BrowserWallet() {
+    return (
+        <div className="min-h-screen bg-gray-900 text-white font-sans">
+            
+                
+        </div>
+    );
+}
