c358f29c82dcfdd2fe55d3ef73de43b64e2979ce
abishek5225 <abishekchaulagain5225@gmail.com>
2025-05-03 23:04:13 +0545

    updated

--- src/app/BrowserWallet/page.jsx
+++ src/app/BrowserWallet/page.jsx
@@ -3,7 +3,9 @@
 export default function BrowserWallet() {
     return (
         <div className="min-h-screen bg-gray-900 text-white font-sans">
-            
+            <div className=" flex flex-col items-center justify-center h-screen">
+            <input type="text" className="bg-gray-600 border rounded h-90 w-100 "/>
+            </div>
                 
         </div>
     );
