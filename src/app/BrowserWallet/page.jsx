760048c9372d0b7425669031891efff18a4a28a3
abishek5225 <abishekchaulagain5225@gmail.com>
2025-05-03 23:19:39 +0545

    updated

--- src/app/BrowserWallet/page.jsx
+++ src/app/BrowserWallet/page.jsx
@@ -3,8 +3,9 @@
 export default function BrowserWallet() {
     return (
         <div className="min-h-screen bg-gray-900 text-white font-sans">
-            <div className=" flex flex-col items-center justify-center h-screen">
-            <input type="text" className="bg-gray-600 border rounded h-90 w-100 "/>
+            <div className=" flex flex-col items-center">
+            <textarea type="text" className="bg-gray-600  rounded h-90 w-100 resize-none"  placeholder="Enter your passpharse here. . ."/>
+            <button className="bg-blue-600 hover:bg-blue-700 mt-5 w-70 h-15 rounded">Unlock Wallet</button>
             </div>
                 
         </div>
